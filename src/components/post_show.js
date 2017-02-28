2da4f382db9d50e64135cf1715657d8d4baf2d53
davide-ravasi <info@davideravasi.com>
2017-02-28 14:00:08 +0100

    added delete component

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
 	}
 
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)			
+		.then(() => {
+				// blog post has been created, navigate the user to the index
+				// we navigate by calling this.context.router.push with the
+				// new path naviagte to
+				// PUSH is a method of redux router
+				this.context.router.push('/');
+			});;
+	}
+
 	render() {
-		return <div>Show post {this.props.params.id}</div>;
+
+		//const post = this.props.post;
+		const { post } = this.props; // equivalent in ecma6
+
+		//if(!this.props.post) {
+		if(!post) {	// equivalent in ecma6
+			return <div>Loading.............</div>;
+		}
+		return (
+			<div>
+				<Link to="/">Back to Index</Link>
+				<button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger pull-sx-right">Delete post
+				</button>
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
+			</div>
+		);
 	}
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+
+function mapStateToProps(state) {
+	return { post: state.posts.post };
+}
+
+export default connect( mapStateToProps, { fetchPost, deletePost })(PostsShow);
